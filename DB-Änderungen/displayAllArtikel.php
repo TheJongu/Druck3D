dc01ce4ed32424a4116a1bd04e75dbb3df16962e
TheJongu <60642840+TheJongu@users.noreply.github.com>
2020-06-16 20:26:06 +0200

    Added Checkboxes and DisplayAllArtikel

---
+++ DB-Änderungen/displayAllArtikel.php
@@ -0,0 +1,69 @@
+<form method="post" action="/Tests/Post/">      
+
+    <?php
+        
+        error_reporting(0);                                             //unterbindet die PHP-eigenen Fehlermeldungen
+
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $db = 'Druck3DDB';
+    
+        $db_link = new mysqli($host, $user, $password, $db);            //Verbindungsaufbau zur Datenbank
+
+        $sqlrequest = 'SELECT Name, Preis, Bildlink, Beschreibung  FROM Artikel';
+
+        $erg = $db_link->query($sqlrequest) or die($db_link->error);    //Liest die Datenbank aus
+
+echo "Connected";
+
+        while ($zeile = $erg->fetch_object())                           //fetch_object liefert ein object, welches die Inhalte der DB-Zeile enthält
+    {
+       ?>
+       
+       <<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+
+th, td {
+  padding: 10px;
+}
+</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Preis</th>
+    <th>Bildlink</th>
+    <th>Beschreibung</th>
+  </tr>
+  <tr>
+    
+    <?php
+  
+  echo "<td>January</td>";
+  echo "<td>January</td>";
+  echo "<td>January</td>";
+  echo "<td>January</td>";
+
+
+    ?>
+
+    </tr>
+
+</table>
+
+</body>
+</html>
+
+      
+    <?php        
+        }
+    ?>
+       
+</form>
