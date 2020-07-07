988076af8778e4c617a6140b5d57d4409f646a40
TheJongu <60642840+TheJongu@users.noreply.github.com>
2020-07-07 16:39:38 +0200

    fct für Warenkorb erstellt (anfragen ungetestet)

---
+++ DB-Changes/Functions/fct_warenkorb.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'fct_sqlconnect.php';
+include_once 'fct_Schlagworte.php';
+
+function removeAllArticleFromUserCart($pk_user) {
+    $sql = 'DELETE FROM warenkorbartikel WHERE warenkorbartikel.FK_Nutzer = ?';
+    $handle = fill_statement($sql, array($pk_user));
+    $handle->execute();
+}
+
+function removeArticleFromUserCart($pk_user, $pk_article) {
+    $sql = 'DELETE FROM warenkorbartikel WHERE warenkorbartikel.FK_Nutzer = ? AND warenkorbartikel.FK_Artikel = ?';
+    $handle = fill_statement($sql, array($pk_user, $pk_article));
+    $handle->execute();
+}
+
+function addArticleToUserCart($pk_user, $pk_article) {
+    $sql = 'INSERT INTO warenkorbartikel (FK_Nutzer, FK_Artikel) VALUES (?, ?)';
+    $handle = fill_statement($sql, array($pk_user, $pk_article));
+    $handle->execute();
+}
+
+?>
+    
