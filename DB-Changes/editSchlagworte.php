f549f1eca8fe21ce6afa109d9a2494d23b4872f7
thejongu <jongu.gamer@gmx.de>
2020-07-26 17:03:37 +0200

    Schlagworte und Editschlagworte kommentierung angepasst

--- DB-Changes/editSchlagworte.php
+++ DB-Changes/editSchlagworte.php
@@ -1,98 +1,84 @@
-    <html>
-        <head>
-            <title>Schlagworte Editieren</title>
-            <meta charset="UTF-8">
-            <style>
-                div.errorDescr{
-                    color: red;
-                    display: none;
-                }
-            </style>
-        </head>
-    </html>
-
-
-    <h1>Schlagwörter</h1>
+<html>
+   <head>
+      <title>Schlagworte Editieren</title>
+      <meta charset="UTF-8">
+      <style>
+         div.errorDescr{
+         color: red;
+         display: none;
+         }
+      </style>
+   </head>
+</html>
+<h1>Schlagwörter</h1>
 <table>
+<?php
+   error_reporting(0);                                             //unterbindet die PHP-eigenen Fehlermeldungen
+   include_once 'Functions/fct_sqlconnect.php';
+   //Alle Schlagworte holen
+   $sql = "SELECT Schlagwort, PK_Schlagwort FROM schlagworte ORDER BY schlagworte.Schlagwort ASC";
+   $handle = fill_statement($sql, array());
+   $handle->execute();
 
-    <?php
-        error_reporting(0);                                             //unterbindet die PHP-eigenen Fehlermeldungen
-        include_once 'Functions/fct_sqlconnect.php';
-        //Alle Schlagworte holen
-        $sql = "SELECT Schlagwort, PK_Schlagwort FROM schlagworte ORDER BY schlagworte.Schlagwort ASC";
-        $handle = fill_statement($sql, array());
-        $handle->execute();
+   //Iteriere über alle möglichen Schlagworte
+   while ($zeile = $handle->fetch(PDO::FETCH_OBJ))                           //fetch_object liefert ein object, welches die Inhalte der DB-Zeile enthält
+   {
+       //Printe Button und Name des Schlagwortes, welches der Button löscht
 
-        //Iteriere über alle möglichen Schlagworte
-        while ($zeile = $handle->fetch(PDO::FETCH_OBJ))                           //fetch_object liefert ein object, welches die Inhalte der DB-Zeile enthält
-        {
-            //Printe Button und Name des Schlagwortes, welches der Button löscht
-
-            ?><form action="editSchlagworteDelete.php" method="get"><?php
-
-            echo "<td><input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'></td>";
-            echo "<td><input type='submit' value='Löschen'/></td>    ";
-            echo "<td>$zeile->Schlagwort</td>";
-            echo "</form>";
-
+       ?>
+<form action="editSchlagworteDelete.php" method="get"><?php
+   echo "<td><input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'></td>";
+   echo "<td><input type='submit' value='Löschen'/></td>    ";
+   echo "<td>$zeile->Schlagwort</td>";
+   echo "</form>";
 
 
-           $zeile = $handle->fetch(PDO::FETCH_OBJ);
-           if($zeile->PK_Schlagwort != NULL){
+   //Ziehe das nächste Element
+   $zeile = $handle->fetch(PDO::FETCH_OBJ);
+   //Prüfe das Element ob Null, wenn nein, gebe nächstes aus.
+   if($zeile->PK_Schlagwort != NULL){
 
-            ?><form action="editSchlagworteDelete.php" method="get"><?php
-            //Printe Button und Name des Schlagwortes, welches der Button löscht
-            echo "<td><input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'></td>";
-            echo "<td><input type='submit' value='Löschen'/></td>    ";
-            echo "<td>$zeile->Schlagwort</td>";
-            echo "</form>";
+   ?>
+<form action="editSchlagworteDelete.php" method="get"><?php
+   //Printe Button und Name des Schlagwortes, welches der Button löscht
+   echo "<td><input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'></td>";
+   echo "<td><input type='submit' value='Löschen'/></td>    ";
+   echo "<td>$zeile->Schlagwort</td>";
+   echo "</form>";
 
-            echo "</tr>";
-            }
+   echo "</tr>";
+   }
+   echo "</tr>";
+   }
+   echo "</table>";
+   ?>
+<form action="newSchlagwortSubmit.php" method="get" id="editSchlagworteForm">
+   <?php
+      echo "<label for='newschlagwort'></label><input name='newschlagwort' id='newschlagwort' type='text' placeholder='Neues Schlagwort' value='' title='Artikelbeschreibung'><div class='errorDescr' id='schlagwortError'>Bitte kein ' eingeben.</div>";
+      echo "<input type='hidden' name='pk_Artikel' value='{$PK_Artikel}'>";
+      ?>
+   <input type="button" value="Füge Schlagwort hinzu" onclick="validate()">
+</form>
+<form action="./displayAllArtikel.php" method="GET">
+   <input type="submit" value="Abbrechen">
+</form>
+<script>
+   function submitForm()
+   {
+       document.getElementById("editSchlagworteForm").submit();
+   }
+   const validate = () => {
+       //RESET UI
+       document.getElementById("schlagwortError").style.display = "none";
 
-            ?>
+       const newschlagwort = document.getElementById("newschlagwort").value;
+       const REGEX = /^[^']*$/;
 
-
-            </tr>
-
-            <?php
-        }
-
-
-        echo "</table>";
-
-        ?>
-
-    <form action="newSchlagwortSubmit.php" method="get" id="editSchlagworteForm">
-        <?php
-        echo "<label for='newschlagwort'></label><input name='newschlagwort' id='newschlagwort' type='text' placeholder='Neues Schlagwort' value='' title='Artikelbeschreibung'><div class='errorDescr' id='schlagwortError'>Bitte kein ' eingeben.</div>";
-        echo "<input type='hidden' name='pk_Artikel' value='{$PK_Artikel}'>";
-        ?>
-        <input type="button" value="Füge Schlagwort hinzu" onclick="validate()">
-    </form>
-
-    <form action="./displayAllArtikel.php" method="GET">
-      <input type="submit" value="Abbrechen">
-    </form>
-    <script>
-        function submitForm()
-        {
-            document.getElementById("editSchlagworteForm").submit();
-        }
-
-        const validate = () => {
-            //RESET UI
-            document.getElementById("schlagwortError").style.display = "none";
-
-            const newschlagwort = document.getElementById("newschlagwort").value;
-            const REGEX = /^[^']*$/;
-
-            if(!REGEX.test(newschlagwort))
-            {
-                document.getElementById("schlagwortError").style.display = "block";
-                return;
-            }
-            submitForm();
-
-        }
-    </script>
+       if(!REGEX.test(newschlagwort))
+       {
+           document.getElementById("schlagwortError").style.display = "block";
+           return;
+       }
+       submitForm();
+   }
+</script>
--- DB-Changes/schlagwortCheckboxes.php
+++ DB-Changes/schlagwortCheckboxes.php
@@ -1,74 +1,73 @@
 <form action="schlagwortCheckboxesSubmit.php" method="GET">
+   <table>
+      <tr>
+         <h1> Schlagworte Hinzufügen</h1>
+      </tr>
+      <?php
+         include_once 'Functions/fct_sqlconnect.php';
+         error_reporting(0);                                             //unterbindet die PHP-eigenen Fehlermeldungen
+         //Ermittle PK Artikel, der bearbeitet werden soll
+         $pk_artikel = $_GET['pk_Artikel'];
+         //Übergebe hidden input, beinhält den Artikel um den es geht
+         echo "<input type='hidden' name='pk_artikel' value='{$pk_artikel}'>";
+         //Alle Schlagworte
+         $sqlGetSchlagworte = "SELECT Schlagwort FROM schlagworte ORDER BY schlagworte.Schlagwort ASC";
+         $handleGetSchlagworte = fill_statement($sqlGetSchlagworte, array());
+         $handleGetSchlagworte->execute();
+         //Alle angehakten Schlagworte
+         $sqlGetSchlagworteForPk = "SELECT DISTINCT schlagworte.Schlagwort FROM artikelschlagworte, schlagworte where artikelschlagworte.FK_Artikel = ? AND artikelschlagworte.FK_Schlagwort = schlagworte.PK_Schlagwort;";
+         $handleGetSchlagworteForPk = fill_statement($sqlGetSchlagworteForPk, array($pk_artikel));
+         $handleGetSchlagworteForPk->execute();
 
-<table>
-  <tr>
-    <h1> Schlagworte Hinzufügen</h1>
-  </tr>
-<?php
-        include_once 'Functions/fct_sqlconnect.php';
-        error_reporting(0);                                             //unterbindet die PHP-eigenen Fehlermeldungen
-        //Ermittle PK Artikel, der bearbeitet werden soll
-        $pk_artikel = $_GET['pk_Artikel'];
-        //Übergebe hidden input, beinhält den Artikel um den es geht
-        echo "<input type='hidden' name='pk_artikel' value='{$pk_artikel}'>";
-        //Alle Schlagworte
-        $sqlGetSchlagworte = "SELECT Schlagwort FROM schlagworte ORDER BY schlagworte.Schlagwort ASC";
-        $handleGetSchlagworte = fill_statement($sqlGetSchlagworte, array());
-        $handleGetSchlagworte->execute();
-        //Alle angehakten Schlagworte
-        $sqlGetSchlagworteForPk = "SELECT DISTINCT schlagworte.Schlagwort FROM artikelschlagworte, schlagworte where artikelschlagworte.FK_Artikel = ? AND artikelschlagworte.FK_Schlagwort = schlagworte.PK_Schlagwort;";
-        $handleGetSchlagworteForPk = fill_statement($sqlGetSchlagworteForPk, array($pk_artikel));
-        $handleGetSchlagworteForPk->execute();
+         //Erstelle ein Array, in welchem alle aktuell angehakten Schlagwörte stehen (von geklicktem Artikel)
+         $currentSchlagworteArray = array();
+         //Iteriere über das SelektErgebnis
+         while ($currentSchlagwort = $handleGetSchlagworteForPk->fetch(PDO::FETCH_OBJ))
+         {
+             //Erweitere das Array um das nächste Schlagwort
+             $currentSchlagworteArray[] = "$currentSchlagwort->Schlagwort";
+         }
+         //Iteriere über alle möglichen Schlagworte
+         $i=0;
 
-        //Erstelle ein Array, in welchem alle aktuell angehakten Schlagwörte stehen (von geklicktem Artikel)
-        $currentSchlagworteArray = array();
-        //Iteriere über das SelektErgebnis
-        while ($currentSchlagwort = $handleGetSchlagworteForPk->fetch(PDO::FETCH_OBJ))
-        {
-            //Erweitere das Array um das nächste Schlagwort
-            $currentSchlagworteArray[] = "$currentSchlagwort->Schlagwort";
-        }
-        //Iteriere über alle möglichen Schlagworte
-        $i=0;
+         while ($zeile = $handleGetSchlagworte->fetch(PDO::FETCH_OBJ))                           //fetch_object liefert ein object, welches die Inhalte der DB-Zeile enthält
+         {
 
-        while ($zeile = $handleGetSchlagworte->fetch(PDO::FETCH_OBJ))                           //fetch_object liefert ein object, welches die Inhalte der DB-Zeile enthält
-        {
+             $isCheckedFlag = "";
+             //Prüfe ob das Aktuelle Schlagwort schon angehakt sein soll
+             foreach ($currentSchlagworteArray as $value) {
+                 if($value == $zeile->Schlagwort){
+                     //Hake an
+                     $isCheckedFlag = "checked";
+                }
+             }
+             //Printe Checkbox, ist Input, dadurch wird an folgende PHP übergeben
+             echo "<td><input $isCheckedFlag  type='checkbox' name='checkboxes[]' value='$zeile->Schlagwort'>{$zeile->Schlagwort}</td>";
+             $i=$i+1;
+             //Hole das zweite Element für die Tabelle
+             $zeile = $handleGetSchlagworte->fetch(PDO::FETCH_OBJ);
+             //Wenn nicht Null, gebe Tabelle weiter aus
+             if($zeile->Schlagwort != null){
 
-            $isCheckedFlag = "";
-            //Prüfe ob das Aktuelle Schlagwort schon angehakt sein soll
-            foreach ($currentSchlagworteArray as $value) {
-                if($value == $zeile->Schlagwort){
-                    //Hake an
-                    $isCheckedFlag = "checked";
-               }
+               $isCheckedFlag = "";
+               //Prüfe ob das Aktuelle Schlagwort schon angehakt sein soll
+                   if($value == $zeile->Schlagwort){
+                     foreach ($currentSchlagworteArray as $value) {
+                       //Hake an
+                       $isCheckedFlag = "checked";
+                     }
+                   }
+               //Printe Checkbox, ist Input, dadurch wird an folgende PHP übergeben
+               echo "<td><input $isCheckedFlag  type='checkbox' name='checkboxes[]' value='$zeile->Schlagwort'>{$zeile->Schlagwort}</td>";
+               $i=$i+1;
             }
-            //Printe Checkbox, ist Input, dadurch wird an folgende PHP übergeben
-            echo "<td><input $isCheckedFlag  type='checkbox' name='checkboxes[]' value='$zeile->Schlagwort'>{$zeile->Schlagwort}</td>";
-            $i=$i+1;
-
-            $zeile = $handleGetSchlagworte->fetch(PDO::FETCH_OBJ);
-            if($zeile->Schlagwort != null){
-
-              $isCheckedFlag = "";
-              //Prüfe ob das Aktuelle Schlagwort schon angehakt sein soll
-                  if($value == $zeile->Schlagwort){
-                    foreach ($currentSchlagworteArray as $value) {
-                      //Hake an
-                      $isCheckedFlag = "checked";
-                    }
-                  }
-              //Printe Checkbox, ist Input, dadurch wird an folgende PHP übergeben
-              echo "<td><input $isCheckedFlag  type='checkbox' name='checkboxes[]' value='$zeile->Schlagwort'>{$zeile->Schlagwort}</td>";
-              $i=$i+1;
+            echo "</tr>";
            }
-           echo "</tr>";
-          }
-    ?>
-    <?php //Wenn Submitbutten gedrückt, öffne schlagwortCheckboxesSubmit.php ?>
-
-  </table>
-<input type="submit" value="Submit now" />
+         ?>
+      <?php //Wenn Submitbutten gedrückt, öffne schlagwortCheckboxesSubmit.php ?>
+   </table>
+   <input type="submit" value="Submit now" />
 </form>
 <form action="./displayAllArtikel.php" method="GET">
-  <input type="submit" value="Abbrechen">
+   <input type="submit" value="Abbrechen">
 </form>
