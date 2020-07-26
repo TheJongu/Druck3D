22cce71ea13398c09a559161185efb7d5471b523
Jakob-Lau <jakob.lauterbach@ingburo.de>
2020-07-26 20:52:17 +0200

    Fixed addusercart.php

--- php/addusercart.php
+++ php/addusercart.php
@@ -6,11 +6,21 @@
     $pk_user = $_GET['pk_nutzer'];
     $pk_article = $_GET['pk_artikel'];
 
-    $sql = "SELECT FK_Nutzer FROM warenkorbartikel WHERE FK_Artikel=?";
-    $handle = fill_statement($sql, array($pk_article));
+    $sql = "SELECT FK_Artikel FROM warenkorbartikel WHERE FK_Nutzer=?";
+    $handle = fill_statement($sql, array($pk_user));
     $handle->execute();
+
+    $article_existing = false;
+
+    while($zeile = $handle->fetch(PDO::FETCH_OBJ))
+    {
+        if($zeile->FK_Artikel == $pk_article)
+        {
+            $article_existing = true;
+        }
+    }
     
-    if($handle->rowCount()==0)
+    if(!$article_existing)
     {
         addArticleToUserCart($pk_user, $pk_article);
     }
