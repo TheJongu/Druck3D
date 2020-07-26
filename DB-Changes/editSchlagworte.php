ac815a4828a2ad96b0df3bd2a276b90745465552
thejongu <jongu.gamer@gmx.de>
2020-07-26 16:50:57 +0200

    Bekacktes PHP outplayed und die Tabelle hinzugefügt
    Tut jetzt

--- DB-Changes/editSchlagworte.php
+++ DB-Changes/editSchlagworte.php
@@ -10,11 +10,12 @@
             </style>
         </head>
     </html>
-    
-    
-    
-    
-    <?php 
+
+
+    <h1>Schlagwörter</h1>
+<table>
+
+    <?php
         error_reporting(0);                                             //unterbindet die PHP-eigenen Fehlermeldungen
         include_once 'Functions/fct_sqlconnect.php';
         //Alle Schlagworte holen
@@ -25,15 +26,43 @@
         //Iteriere über alle möglichen Schlagworte
         while ($zeile = $handle->fetch(PDO::FETCH_OBJ))                           //fetch_object liefert ein object, welches die Inhalte der DB-Zeile enthält
         {
+            //Printe Button und Name des Schlagwortes, welches der Button löscht
+
+            ?><form action="editSchlagworteDelete.php" method="get"><?php
+
+            echo "<td><input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'></td>";
+            echo "<td><input type='submit' value='Löschen'/></td>    ";
+            echo "<td>$zeile->Schlagwort</td>";
+            echo "</form>";
+
+
+
+           $zeile = $handle->fetch(PDO::FETCH_OBJ);
+           if($zeile->PK_Schlagwort != NULL){
+
             ?><form action="editSchlagworteDelete.php" method="get"><?php
             //Printe Button und Name des Schlagwortes, welches der Button löscht
-            echo "<input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'>"; 
-            echo "<input type='submit' value='Löschen'/>    ";
-            echo "$zeile->Schlagwort";
-            ?></form><?php
+            echo "<td><input type='hidden' name='pk_schlagwort' value='{$zeile->PK_Schlagwort}'></td>";
+            echo "<td><input type='submit' value='Löschen'/></td>    ";
+            echo "<td>$zeile->Schlagwort</td>";
+            echo "</form>";
+
+            echo "</tr>";
+            }
+
+            ?>
+
+
+            </tr>
+
+            <?php
         }
+
+
+        echo "</table>";
+
         ?>
-    
+
     <form action="newSchlagwortSubmit.php" method="get" id="editSchlagworteForm">
         <?php
         echo "<label for='newschlagwort'></label><input name='newschlagwort' id='newschlagwort' type='text' placeholder='Neues Schlagwort' value='' title='Artikelbeschreibung'><div class='errorDescr' id='schlagwortError'>Bitte kein ' eingeben.</div>";
@@ -41,13 +70,16 @@
         ?>
         <input type="button" value="Füge Schlagwort hinzu" onclick="validate()">
     </form>
-        
+
+    <form action="./displayAllArtikel.php" method="GET">
+      <input type="submit" value="Abbrechen">
+    </form>
     <script>
         function submitForm()
         {
             document.getElementById("editSchlagworteForm").submit();
         }
-        
+
         const validate = () => {
             //RESET UI
             document.getElementById("schlagwortError").style.display = "none";
@@ -61,11 +93,6 @@
                 return;
             }
             submitForm();
-            
+
         }
     </script>
-        
-        
-        
-
-         
